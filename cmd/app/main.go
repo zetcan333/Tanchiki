b39593c9dcb2393802f6be3f5e8b226081178584
zetcan333 <glebbfufa@gmail.com>
2023-12-31 00:46:25 +0300

    Add Player

---
+++ cmd/app/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+type BattleField struct {
+	Size int
+}
+
+func (p BattleField) AddPlayer(GameMap *[][]string) {
+	i, j := rand.Intn(p.Size), rand.Intn(p.Size)
+	(*GameMap)[i][j] = player
+}
+
+func (p *BattleField) String() string {
+	GameMap := CreateEmptyMap(p.Size)
+	p.AddPlayer(&GameMap)
+	out := strings.Repeat("s ", p.Size+2)
+	out = out[:len(out)-1] + "\n"
+	for i := 0; i < p.Size; i++ {
+		out += "s "
+		out += strings.Join(GameMap[i], " ")
+		out += " s\n"
+	}
+	out += strings.Repeat("s ", p.Size+2)
+	out = out[:len(out)-1] + "\n"
+	out = strings.Replace(out, "s", border, -1)
+	return out
+}
+
+const (
+	border = "0"
+	cell   = "-"
+	player = "@"
+)
+
+func main() {
+	var field BattleField
+	fmt.Scan(&field.Size)
+	fmt.Println(field.String())
+}
+func CreateEmptyMap(x int) [][]string {
+	GameMap := make([][]string, x)
+	for i := range GameMap {
+		GameMap[i] = make([]string, x)
+		for j := range GameMap[i] {
+			GameMap[i][j] = cell
+		}
+	}
+	return GameMap
+}
